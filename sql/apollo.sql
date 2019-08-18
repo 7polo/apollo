2fa793cf34f90fe8d2a3d38f424ff88405e79109
7polo <1368112354@qq.com>
2019-08-18 18:15:08 +0800

    更新sql文件

--- sql/apollo.sql
+++ sql/apollo.sql
@@ -1,17 +1,3 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : 腾讯云
- Source Server Type    : MySQL
- Source Server Version : 50723
- Source Schema         : apollo
-
- Target Server Type    : MySQL
- Target Server Version : 50723
- File Encoding         : 65001
-
- Date: 03/07/2019 07:25:53
-*/
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
@@ -27,7 +13,7 @@ CREATE TABLE `t_catalog` (
   `dir_id` varchar(32) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`uid`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for t_data_dic
@@ -41,7 +27,16 @@ CREATE TABLE `t_data_dic` (
   `value` text,
   `type` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`uid`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of t_data_dic
+-- ----------------------------
+BEGIN;
+INSERT INTO `t_data_dic` VALUES ('1b6450d10f707b2eeed6e83919149289', '2019-06-16 14:29:09', b'0', '7polo github', 'https://github.com/7polo', 'friend_link');
+INSERT INTO `t_data_dic` VALUES ('45d1c1cf814d4dba90819520aafe3061', '2019-07-15 12:01:22', b'0', '轮播图', 'https://github.com/7polo/resources/blob/master/apollo/carousel.png?raw=true', 'carousel');
+INSERT INTO `t_data_dic` VALUES ('f644a90153e19381384154debe41d3ac', '2019-06-16 14:28:23', b'1', '轮播图', 'https://github.com/7polo/resources/blob/master/apollo/carousel.jpg?raw=true', 'carousel1');
+COMMIT;
 
 -- ----------------------------
 -- Table structure for t_log_record
@@ -75,8 +70,9 @@ CREATE TABLE `t_note` (
   `publish_dt` datetime DEFAULT NULL,
   `read_count` int(11) DEFAULT NULL,
   `good` int(11) DEFAULT NULL,
+  `md_html` text,
   PRIMARY KEY (`uid`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for t_seo
@@ -104,7 +100,7 @@ CREATE TABLE `t_site_map` (
   `name` varchar(255) DEFAULT NULL COMMENT '关联类型',
   `content` text NOT NULL COMMENT '关联id',
   PRIMARY KEY (`uid`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for t_sys_config
@@ -125,6 +121,13 @@ CREATE TABLE `t_sys_config` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
+-- Records of t_sys_config
+-- ----------------------------
+BEGIN;
+INSERT INTO `t_sys_config` VALUES ('e028ceba96e7ac85c2e4336ffd09ee91', '2019-08-18 18:03:41', b'0', NULL, '', NULL, NULL, NULL, NULL);
+COMMIT;
+
+-- ----------------------------
 -- Table structure for t_tag
 -- ----------------------------
 DROP TABLE IF EXISTS `t_tag`;
@@ -135,7 +138,7 @@ CREATE TABLE `t_tag` (
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`uid`) USING BTREE,
   UNIQUE KEY `name` (`name`,`deleted`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for t_tag_note
@@ -150,8 +153,7 @@ CREATE TABLE `t_tag_note` (
   PRIMARY KEY (`uid`) USING BTREE,
   KEY `note_id` (`note_id`) USING BTREE,
   KEY `tag_id` (`tag_id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for t_user
@@ -165,6 +167,9 @@ CREATE TABLE `t_user` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `salt` varchar(255) DEFAULT NULL,
+  `icon` text COMMENT '图片的base64',
+  `skills` varchar(500) DEFAULT NULL,
+  `saying` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`uid`) USING BTREE,
   UNIQUE KEY `name` (`username`,`deleted`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -173,8 +178,7 @@ CREATE TABLE `t_user` (
 -- Records of t_user
 -- ----------------------------
 BEGIN;
-INSERT INTO `t_user` VALUES ('1135568155192750081', '2019-06-03 15:25:21', b'0', NULL, 'admin', 'e0ac3a60e860fbe8b13f69e3dd7f9ca4', 'f44bd641-38e7-4917-b686-dfe50b2c295f');
+INSERT INTO `t_user` VALUES ('1135568155192750081', '2019-06-03 15:25:21', b'0', NULL, 'admin', '0e3ac7ddeef8307d357d87fbbd61a85e', '7cf10786-a2e9-49eb-9c26-6234ff81c172', NULL, NULL, NULL);
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
-
